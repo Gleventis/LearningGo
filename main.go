645a0b3596d11b0ad17c420bb934a70862676456
George Leventis <--show-origin>
2025-11-16 21:30:07 +0100

    Second iteration
    Added routes for viewing and creating snippets

--- main.go
+++ main.go
@@ -7,13 +7,28 @@ import (
 
 // First we define a handler function for the default landing url (/)
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
 }
 
 func main() {
 	mux := http.NewServeMux() // initialize a new servemux
 
 	mux.HandleFunc("/", home) // register the home function as the handler for the "/" page
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
 
